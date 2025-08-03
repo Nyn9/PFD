76009eb16a60785f3754851192127e468b24e058
nferrad <nferrad@student.42.fr>
2025-08-03 05:46:22 +0200

    m00/ex06

---
+++ m00/ex06/filterstring.py
@@ -0,0 +1,21 @@
+import sys
+from ft_filter import ft_filter
+
+def main():
+	av = sys.argv
+	ac = len(av)
+	try:
+		if ac != 3:
+			raise AssertionError("the arguments are bad")
+		if ((not av[1].isalpha() and av[1].isspace()) or not av[2].isdigit()):
+			raise AssertionError("the arguments are bad")
+		s = av[1].split(' ')
+		length = int(av[2])
+		word = [w for w in s if w.isalpha()]
+		print(list(ft_filter(lambda x: len(x) > length, word)))
+	except AssertionError as e:
+		print(f"AssertionError: {e}")
+		sys.exit(1)
+
+if __name__ == "__main__":
+	main()
---
+++ m00/ex06/ft_filter.py
@@ -0,0 +1,23 @@
+import sys
+
+def ft_filter(function, iterable):
+	"""Returns an iterator that yields the items from the iterable for which function(item) returns True."""
+	try:
+		if not hasattr(iterable, '__iter__'):
+			raise TypeError(f"'{type(iterable).__name__}' object is not iterable")
+		if function is None:
+			return (item for item in iterable if item)
+		return (item for item in iterable if function(item))
+	except TypeError as e:
+		print(f"TypeError: {e}")
+		sys.exit(1)
+
+def main():
+	"""Example usage of ft_filter function."""''
+	data = [1, 2, 3, 4, 5]
+	n = 5
+	print(list(ft_filter(lambda x: x > 2, data)))
+	print(list(ft_filter(lambda x: x > 2, n)))
+
+if __name__ == "__main__":
+	main()
